1ffc96b3a0d442ad684b2cc4dce37e44a349b252
Grymer <pronicoxd@gmail.com>
2023-10-22 21:19:06 +0200

    Update lib/mappers/worktask-mapper.ts

--- lib/mappers/worktask-mapper.ts
+++ lib/mappers/worktask-mapper.ts
@@ -8,7 +8,11 @@ interface Worktask {
   hours: number;
 }
 
-const checklistToWorktask = {
+
+type CheckListMapper = {
+[key:string]: number[] }
+
+const checklistToWorktask: CheckListMapper = {
   type1: [1, 2, 3],
   type2: [2, 4, 6],
   type3: [3, 6, 9],
